d9525f34d0e344cefaf179f9ed421c23a3950d98
omkar <omkardudhewar121@gmail.com>
2022-12-15 17:34:10 +0530

    added: created binary search tree

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -34,5 +34,6 @@ public class BinarySearchTree {
 
     public void printData() {
         print(root);
+
     }
 }
--- BinarySearchTreeMain.java
+++ BinarySearchTreeMain.java
@@ -4,9 +4,44 @@ public class BinarySearchTreeMain {
     public static void main(String[] args) {
         BinarySearchTree binarySearchTree = new BinarySearchTree();
         binarySearchTree.addData(56);
+        binarySearchTree.printData();
+        System.out.println();
         binarySearchTree.addData(30);
+        binarySearchTree.printData();
+        System.out.println();
         binarySearchTree.addData(70);
         binarySearchTree.printData();
+        System.out.println();
+        binarySearchTree.addData(22);
+        binarySearchTree.printData();
+        System.out.println();
+        binarySearchTree.addData(40);
+        binarySearchTree.printData();
+        System.out.println();
+        binarySearchTree.addData(11);
+        binarySearchTree.printData();
+        System.out.println();
+        binarySearchTree.addData(3);
+        binarySearchTree.printData();
+        System.out.println();
+        binarySearchTree.addData(16);
+        binarySearchTree.printData();
+        System.out.println();
+        binarySearchTree.addData(60);
+        binarySearchTree.printData();
+        System.out.println();
+        binarySearchTree.addData(95);
+        binarySearchTree.printData();
+        System.out.println();
+        binarySearchTree.addData(65);
+        binarySearchTree.printData();
+        System.out.println();
+        binarySearchTree.addData(63);
+        binarySearchTree.printData();
+        System.out.println();
+        binarySearchTree.addData(68);
+        binarySearchTree.printData();
+
 
 
     }
