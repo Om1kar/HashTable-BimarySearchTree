66e621fbfec39bec22af391995b4c6f904c525c7
omkar <omkardudhewar121@gmail.com>
2022-12-15 15:08:53 +0530

    added: create bst by adding elements

---
+++ BinarySearchTree.java
@@ -0,0 +1,38 @@
+package BinarySearchTree;
+
+public class BinarySearchTree {
+    Node root;
+
+    public Node insert(Node root, int data) {
+        /*
+         * add root and data
+         */
+        if (root == null) {
+            root = new Node(data);
+            return root;
+
+        } else if (data < root.data) {
+            root.left = insert(root.left, data);
+        } else {
+            root.right = insert(root.right, data);
+        }
+        return root;
+    }
+
+    public void addData(int data) {
+        root = insert(root, data);
+    }
+
+    public void print(Node root) {
+        if (root == null) {
+            return;
+        }
+        print(root.left);
+        System.out.print(root.data + " ");
+        print(root.right);
+    }
+
+    public void printData() {
+        print(root);
+    }
+}
---
+++ BinarySearchTreeMain.java
@@ -0,0 +1,13 @@
+package BinarySearchTree;
+
+public class BinarySearchTreeMain {
+    public static void main(String[] args) {
+        BinarySearchTree binarySearchTree = new BinarySearchTree();
+        binarySearchTree.addData(56);
+        binarySearchTree.addData(30);
+        binarySearchTree.addData(70);
+        binarySearchTree.printData();
+
+
+    }
+}
